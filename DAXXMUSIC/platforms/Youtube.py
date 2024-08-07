9e20bae1b40c8be80e79846eff1d87e2c2e8849c
ishu9805 <abhishek47903@gmail.com>
2024-08-07 13:53:10 +0530

    Update Youtube.py

--- DAXXMUSIC/platforms/Youtube.py
+++ DAXXMUSIC/platforms/Youtube.py
@@ -6,7 +6,7 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from youtubesearchpython.__future__ import VideosSearch
+from youtubesearchpython import VideosSearch
 
 from DAXXMUSIC.utils.database import is_on_off
 from DAXXMUSIC.utils.formatters import time_to_seconds
